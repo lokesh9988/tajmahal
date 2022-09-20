d151cb031758219a2662bba14daec312a2f32759
lokesh <lokeshbaraskar1995@gmail.com>
2022-09-20 18:36:27 +0530

    delete hello github

--- src/main/java/MYTestNG/NewprojectwithtestNG/dashboard.java
+++ src/main/java/MYTestNG/NewprojectwithtestNG/dashboard.java
@@ -5,7 +5,7 @@ public class dashboard {
 	public static void main(String[] args) {
 		
 		System.out.println("dashboard panal");
-		System.out.println("hello github");
+		System.out.println(" github");
 			
 		}
 	}
